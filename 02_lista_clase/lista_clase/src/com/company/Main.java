b7b3aa97c186082af72b8e2ffefdeec3d4fa336f
Unknown <mikel.aretxabaleta@gmail.com>
2017-11-20 20:06:24 +0100

    Hasta el 2

--- 02_lista_clase/lista_clase/src/com/company/Main.java
+++ 02_lista_clase/lista_clase/src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+	final int ALUMNOS=3;
+	String[] nombre=new String[ALUMNOS];
+	String buscar;
+
+	do {
+        for (int i = 0; i <ALUMNOS ; i++) {
+            System.out.print("Nombre y apellidos: ");
+            nombre[i]=br.readLine();
+        }
+        System.out.println("Buscar letra: ");
+        buscar=br.readLine();
+
+        for (int i = 0; i <ALUMNOS ; i++) {
+            if (nombre[i].charAt(0)==buscar.charAt(0)) System.out.println(nombre[i]);
+        }
+
+    }while(!buscar.equalsIgnoreCase("fin"));
     }
 }
